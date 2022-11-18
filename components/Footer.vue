e227b3758c45b1ad411759e07c06f0d0499bb654
troyciesco <tmciesco@gmail.com>
2022-11-17 19:16:23 -0500

    footer text size

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="p-4 flex flex-col items-center justify-center font-mono">
+  <footer class="p-4 text-center font-mono text-sm">
     <p>Made with 🍕 in New England.</p>
     <p>© {{ new Date().getFullYear() }} Troy Ciesco. All rights reserved.</p>
   </footer>
