a824eeede7f1132662670bde9127163cd1a178a6
troyciesco <tmciesco@gmail.com>
2022-11-21 11:33:42 -0500

    remove about page for now, mobile header

--- components/Header.vue
+++ components/Header.vue
@@ -2,23 +2,14 @@
   const route = useRoute()
 </script>
 <template>
-  <header class="p-8 grid grid-cols-5 justify-between items-center">
+  <header class="grid items-center justify-between grid-cols-3 p-4 md:p-8 md:grid-cols-4">
     <NuxtLink
       to="/"
-      class="font-logo col-span-1 text-3xl font-bold tracking-wide"
+      class="col-span-1 text-3xl font-bold tracking-wide font-logo"
       ><span>{</span><span class="text-secondary-600">tmc</span><span>}</span></NuxtLink
     >
-    <nav class="flex justify-center col-span-3">
-      <ul class="flex gap-8 items-center font-mono">
-        <!-- <li><a href="/">Now</a></li>
-        <li><a href="/">Work</a></li>
-        <li><a href="/">Book Reviews</a></li>
-        <li><a href="/">Mission</a></li>
-        <li><a href="/">Resources</a></li>
-        <li><a href="/">Uses</a></li>
-        <li><a href="/">Code Snippets</a></li>
-        <li><a href="/store">Store</a></li>
-        <li><a href="/">Speaking</a></li> -->
+    <nav class="flex col-span-1 md:justify-center md:col-span-2">
+      <ul class="flex items-center gap-8 font-mono">
         <li>
           <NuxtLink
             to="/"
@@ -31,13 +22,6 @@
             to="/blog"
             :class="{ 'underline decoration-secondary-500 font-bold': route.path.includes('/blog') }"
             >Blog</NuxtLink
-          >
-        </li>
-        <li>
-          <NuxtLink
-            to="/about"
-            :class="{ 'underline decoration-secondary-500 font-bold': route.path === '/about' }"
-            >About</NuxtLink
           >
         </li>
       </ul>
